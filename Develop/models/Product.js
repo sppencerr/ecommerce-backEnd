60d2d3328363e6efbfc52e0cfd98a0481e512c22
Spencer Gerritsen <spencergerritsen@gmail.com>
2022-11-17 11:04:45 -0700

    dev folder

--- Develop/models/Product.js
+++ Develop/models/Product.js
@@ -10,37 +10,6 @@ class Product extends Model {}
 Product.init(
   {
     // define columns
-    id:{
-      type: DataTypes.INTEGER,
-      allowNull: false,
-      primaryKey: true,
-      autoIncrement: true
-    },
-    product_name: {
-      type: DataTypes.STRING,
-      allowNull: false
-    },
-    price: {
-      type: DataTypes.DECIMAL(10, 2),
-      allowNull: false,
-      validate: {
-        isDecimal: true
-      }
-    },
-    stock: {
-      type: DataTypes.INTEGER,
-      allowNull: false,
-      defaultValue: 10,
-      isNumeric: true
-    },
-    category_id: {
-      type: DataTypes.INTEGER,
-      references: {
-        model: 'category',
-        key: 'id',
-        unique: false
-      }
-    }
   },
   {
     sequelize,
