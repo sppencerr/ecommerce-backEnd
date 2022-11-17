ca776a509668cde6d469a0c987acef9dec30365e
Spencer Gerritsen <spencergerritsen@gmail.com>
2022-11-17 11:10:04 -0700

    tag

--- Develop/models/ProductTag.js
+++ Develop/models/ProductTag.js
@@ -6,7 +6,29 @@ class ProductTag extends Model {}
 
 ProductTag.init(
   {
-    // define columns
+    id: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      primaryKey: true,
+      autoIncrement: true,
+    },
+    product_id: {
+      type: DataTypes.INTEGER,
+
+      //References the Product model's id.  
+      references: {
+        model: 'product',
+        key: 'id',
+      }
+    },
+    //References the Tag model's id.
+    tag_id: {
+      type: DataTypes.INTEGER,
+      references: {
+        model: 'tag',
+        key: 'id'
+      }
+    }
   },
   {
     sequelize,
